6c5fd850844f36afdb6136c4ad7b52168c13f20b
namtran10 <hoangnamtran.it@gmail.com>
2024-01-15 17:22:55 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,3 +1,4 @@
 #!/bin/sh -l
+export PATH="/Users/namtran/.nvm/versions/node/v20.10.0/bin/node"
 node /index.js --openai-api-key "$1" --github-token "$2" --github-pr-id "$3" --dev-lang "$4" --openai-engine "$5" --openai-temperature "$6" --openai-max-tokens "$7"
 
