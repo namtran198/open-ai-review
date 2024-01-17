d4653a2467bb61eaf8b779221d9ee45ad8aa95da
namtran10 <hoangnamtran.it@gmail.com>
2024-01-17 09:13:11 +0700

    Test open AI key

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const OpenAI = require("openai");
 
 async function codeReview(parameters) {
   const octokit = new Octokit({ auth: parameters.github_token });
-  console.log('parameters.openai_api_key', parameters.openai_api_key)
-  const openai = new OpenAI({ key: parameters.openai_api_key });
+  const openai = new OpenAI({ key: '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' });
 
   const repo = await octokit.repos.get({
     owner: process.env.GITHUB_REPOSITORY_OWNER,
